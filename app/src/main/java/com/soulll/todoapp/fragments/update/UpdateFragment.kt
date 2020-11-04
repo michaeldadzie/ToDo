bbc627c4e28a78dc1549dacf4cf096db291d4e0d
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-04 01:23:48 +0000

    Initial commit

---
+++ app/src/main/java/com/soulll/todoapp/fragments/update/UpdateFragment.kt
@@ -0,0 +1,20 @@
+package com.soulll.todoapp.fragments.update
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.soulll.todoapp.R
+
+class UpdateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+}
