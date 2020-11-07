3907fcafdf4617229d8b69bc5a9a60506d8cf8c9
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-07 00:43:25 +0000

    Added ToDoDatabase

---
+++ app/src/main/java/com/soulll/todoapp/data/ToDoDatabase.kt
@@ -0,0 +1,43 @@
+package com.soulll.todoapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.soulll.todoapp.data.models.ToDoData
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+
+@Database(entities = [ToDoData::class], version = 1, exportSchema = false)
+@TypeConverters(Converter::class)
+abstract class ToDoDatabase: RoomDatabase() {
+
+    abstract fun toDoDao(): ToDoDao
+
+    companion object {
+
+        @Volatile
+        private  var INSTANCE: ToDoDatabase? = null
+
+
+        fun getDatabase(context: Context): ToDoDatabase {
+            val tempInstance = INSTANCE
+            if(tempInstance != null) {
+                return  tempInstance
+            }
+
+            kotlin.synchronized(this) {
+                val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ToDoDatabase::class.java,
+                        "todo_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+    }
+
+}
