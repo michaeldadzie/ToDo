74dec22a2c0a96a656196ea74591f8d11976cf9c
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-07 00:42:36 +0000

    Added ToDoViewModel

---
+++ app/src/main/java/com/soulll/todoapp/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,50 @@
+package com.soulll.todoapp.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.soulll.todoapp.data.ToDoDatabase
+import com.soulll.todoapp.data.models.ToDoData
+import com.soulll.todoapp.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application):AndroidViewModel(application) {
+
+    private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
+
+    private val repository: ToDoRepository
+
+    val getAllData: LiveData<List<ToDoData>>
+
+    init {
+        repository = ToDoRepository(toDoDao)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData) {
+        viewModelScope.launch (Dispatchers.IO){
+            repository.insertData(toDoData)
+        }
+    }
+
+    fun updateData(toDoData: ToDoData){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.updateData(toDoData)
+        }
+    }
+
+    fun deleteItem(toDoData: ToDoData){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.deleteItem(toDoData)
+        }
+    }
+
+    fun deleteAll() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteAll()
+        }
+    }
+
+}
