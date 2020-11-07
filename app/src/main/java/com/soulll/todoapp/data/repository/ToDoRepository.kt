5284cf442fb0d0c0df275eb9feb40fa6e06adff5
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-07 00:42:14 +0000

    Added Delete Item, Update Data and Delete All and Add Functions

---
+++ app/src/main/java/com/soulll/todoapp/data/repository/ToDoRepository.kt
@@ -0,0 +1,27 @@
+package com.soulll.todoapp.data.repository
+
+import androidx.lifecycle.LiveData
+import com.soulll.todoapp.data.ToDoDao
+import com.soulll.todoapp.data.models.ToDoData
+
+class ToDoRepository(private val toDoDao: ToDoDao) {
+
+    val getAllData: LiveData<List<ToDoData>> = toDoDao.getAllData()
+
+    suspend fun insertData(toDoData: ToDoData) {
+        toDoDao.insertData(toDoData)
+    }
+
+    suspend fun updateData(toDoData: ToDoData){
+        toDoDao.updateData(toDoData)
+    }
+
+    suspend fun deleteItem(toDoData: ToDoData){
+        toDoDao.deleteItem(toDoData)
+    }
+
+    suspend fun deleteAll() {
+        toDoDao.deleteAll()
+    }
+
+}
