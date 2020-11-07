aa1519124325d06bc17a7259b75915268d2042cb
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-07 00:50:42 +0000

    Improve UI Code

--- app/src/main/java/com/soulll/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/soulll/todoapp/fragments/list/ListFragment.kt
@@ -1,13 +1,28 @@
 package com.soulll.todoapp.fragments.list
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.soulll.todoapp.R
+import com.soulll.todoapp.data.viewmodel.ToDoViewModel
+import com.soulll.todoapp.fragments.SharedViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
+
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+
+    private val mSharedViewModel: SharedViewModel by viewModels()
+
+    private val adapter: ListAdapter by lazy { ListAdapter() }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -15,21 +30,57 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
 
+        val recyclerView = view.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+            mSharedViewModel.checkIfDatabaseEmpty(data)
+            adapter.setData(data)
+        })
+
+        mSharedViewModel.emptyDatabase.observe(viewLifecycleOwner, Observer {
+            showEmptyDatabaseView(it)
+        })
+
         view.floatingActionButton.setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
-        view.list_layout.setOnClickListener {
-            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
-        }
-
         setHasOptionsMenu(true)
-
         return view
+    }
+
+    private fun showEmptyDatabaseView(emptyDataBase: Boolean) {
+        if (emptyDataBase){
+            view?.no_data_imageView?.visibility = View.VISIBLE
+            view?.no_data_textView?.visibility = View.VISIBLE
+        }else{
+            view?.no_data_imageView?.visibility = View.INVISIBLE
+            view?.no_data_textView?.visibility = View.INVISIBLE
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_delete_all)
+            confirmRemoval()
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun confirmRemoval() {
+        var builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes"){_, _ ->
+            mToDoViewModel.deleteAll()
+            Toast.makeText(requireContext(),"Successfully Removed Everything", Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("No"){_, _ ->}
+        builder.setTitle("Delete Everything?")
+        builder.setMessage("Are you sure you want to remove Everything?")
+        builder.create().show()
     }
 
 }
