f93a4c093fb1da1603c033f525ca18e8016587c5
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-08 23:09:52 +0000

    Moved List Adapter to adapter package

---
+++ app/src/main/java/com/soulll/todoapp/fragments/list/adapter/ListAdapter.kt
@@ -0,0 +1,48 @@
+package com.soulll.todoapp.fragments.list.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.soulll.todoapp.data.models.ToDoData
+import com.soulll.todoapp.databinding.RowLayoutBinding
+
+class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    var dataList = emptyList<ToDoData>()
+
+    class MyViewHolder(private val binding: RowLayoutBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(toDoData: ToDoData){
+            binding.toDoData = toDoData
+            binding.executePendingBindings()
+        }
+
+        companion object{
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val currentItem = dataList[position]
+        holder.bind(currentItem)
+
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    fun setData(toDoData: List<ToDoData>){
+        this.dataList = toDoData
+        notifyDataSetChanged()
+    }
+
+}
