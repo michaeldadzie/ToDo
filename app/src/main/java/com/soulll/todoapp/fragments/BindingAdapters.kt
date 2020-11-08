2878d21197230cb615412dc5c49db8dcf36d49e5
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-08 21:46:37 +0000

    Added BindingAdapters

---
+++ app/src/main/java/com/soulll/todoapp/fragments/BindingAdapters.kt
@@ -0,0 +1,46 @@
+package com.soulll.todoapp.fragments
+
+import android.view.View
+import android.widget.Spinner
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.soulll.todoapp.R
+import com.soulll.todoapp.data.models.Priority
+
+class BindingAdapters {
+
+    companion object{
+        @BindingAdapter("android:navigateToAddFragment")
+        @JvmStatic
+        fun navigateToAddFragment(view: FloatingActionButton, navigate: Boolean){
+            view.setOnClickListener{
+                if (navigate) {
+                    view.findNavController().navigate(R.id.action_listFragment_to_addFragment)
+                }
+            }
+        }
+
+        @BindingAdapter("android:emptyDatabase")
+        @JvmStatic
+        fun emptyDatabase(view: View,emptyDatabase: MutableLiveData<Boolean>){
+            when(emptyDatabase.value){
+                true -> view.visibility = View.VISIBLE
+                false -> view.visibility = View.INVISIBLE
+            }
+        }
+
+        @BindingAdapter("android:parsePriorityToInt")
+        @JvmStatic
+        fun parsePriorityToInt(view: Spinner, priority: Priority) {
+            when(priority) {
+                Priority.HIGH -> { view.setSelection(0) }
+                Priority.MEDIUM -> { view.setSelection(1) }
+                Priority.LOW -> { view.setSelection(2) }
+            }
+        }
+
+    }
+
+}
