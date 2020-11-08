3517592217ffa807b8b8e5a1d18147b1a1ba84db
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-08 23:08:54 +0000

    Added Parse PriorityColor & sendDataToUpdateFragment

--- app/src/main/java/com/soulll/todoapp/fragments/BindingAdapters.kt
+++ app/src/main/java/com/soulll/todoapp/fragments/BindingAdapters.kt
@@ -1,13 +1,19 @@
 package com.soulll.todoapp.fragments
 
+import android.graphics.Color.red
 import android.view.View
 import android.widget.Spinner
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.soulll.todoapp.R
+import com.soulll.todoapp.R.*
 import com.soulll.todoapp.data.models.Priority
+import com.soulll.todoapp.data.models.ToDoData
+import com.soulll.todoapp.fragments.list.ListFragmentDirections
 
 class BindingAdapters {
 
@@ -17,7 +23,7 @@ class BindingAdapters {
         fun navigateToAddFragment(view: FloatingActionButton, navigate: Boolean){
             view.setOnClickListener{
                 if (navigate) {
-                    view.findNavController().navigate(R.id.action_listFragment_to_addFragment)
+                    view.findNavController().navigate(id.action_listFragment_to_addFragment)
                 }
             }
         }
@@ -38,6 +44,25 @@ class BindingAdapters {
                 Priority.HIGH -> { view.setSelection(0) }
                 Priority.MEDIUM -> { view.setSelection(1) }
                 Priority.LOW -> { view.setSelection(2) }
+            }
+        }
+
+        @BindingAdapter("android:parsePriorityColor")
+        @JvmStatic
+        fun parsePriorityColor(cardView: CardView, priority: Priority){
+            when(priority) {
+                Priority.HIGH -> { cardView.setCardBackgroundColor(cardView.context.getColor(R.color.red_500)) }
+                Priority.MEDIUM -> { cardView.setCardBackgroundColor(cardView.context.getColor(R.color.yellow_500)) }
+                Priority.LOW -> { cardView.setCardBackgroundColor(cardView.context.getColor(R.color.green_500)) }
+            }
+        }
+
+        @BindingAdapter("android:sendDataToUpdateFragment")
+        @JvmStatic
+        fun sendDataToUpdateFragment(view: ConstraintLayout, currentItem: ToDoData){
+            view.setOnClickListener {
+                val action = ListFragmentDirections.actionListFragmentToUpdateFragment(currentItem)
+                view.findNavController().navigate(action)
             }
         }
 
