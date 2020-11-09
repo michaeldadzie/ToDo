3f927b8b7a4ad61f31d5b5570a7bb81bf7e37519
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-09 22:29:22 +0000

    Added ToDoDiffUtil

---
+++ app/src/main/java/com/soulll/todoapp/fragments/list/adapter/ToDoDiffUtil.kt
@@ -0,0 +1,27 @@
+package com.soulll.todoapp.fragments.list.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import com.soulll.todoapp.data.models.ToDoData
+
+class ToDoDiffUtil(private val oldList: List<ToDoData>,
+                   private val newList: List<ToDoData>
+): DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+       return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] === newList[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].id == newList[newItemPosition].id
+                && return oldList[oldItemPosition].title == newList[newItemPosition].title
+                &&return oldList[oldItemPosition].description == newList[newItemPosition].description
+                &&return oldList[oldItemPosition].priority == newList[newItemPosition].priority
+    }
+}
