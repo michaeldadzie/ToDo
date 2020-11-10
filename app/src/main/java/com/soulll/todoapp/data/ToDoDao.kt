8e939d9972d58039e796246209a0aebcb5aafabf
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-10 11:31:10 +0000

    Sort Data By High or Low Priority

--- app/src/main/java/com/soulll/todoapp/data/ToDoDao.kt
+++ app/src/main/java/com/soulll/todoapp/data/ToDoDao.kt
@@ -24,4 +24,9 @@ interface ToDoDao {
     @Query("SELECT * FROM todo_table WHERE title LIKE :searchQuery")
     fun searchDatabase(searchQuery: String): LiveData<List<ToDoData>>
 
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN 'H%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'L%' THEN 3 END")
+    fun sortByHighPriority(): LiveData<List<ToDoData>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
+    fun sortByLowPriority(): LiveData<List<ToDoData>>
 }
--- app/src/main/java/com/soulll/todoapp/data/viewmodel/ToDoViewModel.kt
+++ app/src/main/java/com/soulll/todoapp/data/viewmodel/ToDoViewModel.kt
@@ -19,9 +19,14 @@ class ToDoViewModel(application: Application):AndroidViewModel(application) {
 
     val getAllData: LiveData<List<ToDoData>>
 
+    val sortByHighPriority: LiveData<List<ToDoData>>
+    val sortByLowPriority: LiveData<List<ToDoData>>
+
     init {
         repository = ToDoRepository(toDoDao)
         getAllData = repository.getAllData
+        sortByHighPriority = repository.sortByHighPriority
+        sortByLowPriority = repository.sortByLowPriority
     }
 
     fun insertData(toDoData: ToDoData) {
