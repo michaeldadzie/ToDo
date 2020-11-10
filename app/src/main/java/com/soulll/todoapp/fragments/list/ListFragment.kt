8fb2f756c8136195e54e5e4fcd4826e351b1269f
Michael Dadzie <dadziemikke2@gmail.com>
2020-11-10 11:33:26 +0000

    Implemented Data Sort

--- app/src/main/java/com/soulll/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/soulll/todoapp/fragments/list/ListFragment.kt
@@ -3,14 +3,13 @@ package com.soulll.todoapp.fragments.list
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.GridLayout
 import androidx.appcompat.widget.SearchView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.*
 import com.google.android.material.snackbar.Snackbar
 import com.soulll.todoapp.R
 import com.soulll.todoapp.data.models.ToDoData
@@ -50,13 +49,14 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener {
         })
 
         setHasOptionsMenu(true)
+
         return binding.root
     }
 
     private fun setUpRecyclerView() {
         val recyclerView = binding.recyclerView
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.itemAnimator = SlideInUpAnimator().apply {
             addDuration = 300
         }
@@ -73,14 +73,14 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener {
                 mToDoViewModel.deleteItem(deletedItem)
                 adapter.notifyItemRemoved(viewHolder.adapterPosition)
                 // RestoredDeletedData
-                restoreDeletedData(viewHolder.itemView, deletedItem, viewHolder.adapterPosition)
+                restoreDeletedData(viewHolder.itemView, deletedItem)
             }
         }
         val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
         itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 
-    private fun restoreDeletedData(view: View, deletedItem: ToDoData, position: Int){
+    private fun restoreDeletedData(view: View, deletedItem: ToDoData){
         val snackBar =  Snackbar.make(
             view,
             "Deleted '${deletedItem.title}'",
@@ -103,8 +103,11 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu_delete_all)
-            confirmRemoval()
+        when (item.itemId){
+            R.id.menu_delete_all ->  confirmRemoval()
+            R.id.menu_priority_high -> mToDoViewModel.sortByHighPriority.observe(this, Observer { adapter.setData(it) })
+            R.id.menu_priority_low -> mToDoViewModel.sortByLowPriority.observe(this, Observer { adapter.setData(it) })
+        }
         return super.onOptionsItemSelected(item)
     }
 
